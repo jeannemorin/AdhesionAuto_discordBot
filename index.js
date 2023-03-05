c3fffd727f10f992197d55cbca78299cb181bb8d
jeannemorin <46577907+jeannemorin@users.noreply.github.com>
2023-03-05 15:36:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,15 +7,37 @@ const client = new Client({
   ],
 });
 
+const myGuildID = "1081268387604336781";
+let myGuild = null;
+const roleID = "1081313810851319838";
+const memberName = "Jeaaaaaannne !";
+
+function setGuild() {
+  myGuild = client.guilds.cache.at(0);
+}
+
+function getMember() {
+  const user_id = client.users.cache.find(user => user.username == memberName).id;
+  const member = myGuild.members.cache.get(user_id);
+
+  return member;
+}
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
+  setGuild()
 })
 
 client.on("messageCreate", (message) => {
-  if (message.content === "ping")
+  if (message.content === "ping") {
     message.reply(({
       content: "pong",
     }))
+
+    const member = getMember()
+    member.roles.add(roleID)
+  }
+  
 })
 
 client.login(process.env['TOKEN'])
