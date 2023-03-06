7a92d260aef87c8174d5b09427d253e7eaf4fd09
jeannemorin <46577907+jeannemorin@users.noreply.github.com>
2023-03-06 20:41:05 +0100

    Link API and role giving module

--- index.js
+++ index.js
@@ -1,24 +1,60 @@
 const { Client, GatewayIntentBits } = require('discord.js')
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+let info = null;
+
+app.listen(8080, () => console.log('Music sounds better with yooooouuuu'));
+
+app.get('/', (req, res) => {
+  res.send("Hello World !")
+});
+
+app.post('/action', (req, res) => {
+  let body = '';
+  req.on('data', chunk => {
+    body += chunk.toString();
+  });
+  req.on('end', () => {
+    const reqparse = JSON.parse(body);
+    if (reqparse.eventType == 'Order')
+      info = reqparse;
+
+    console.log(info.data.items[0].customFields[0].answer)
+    const memberName = info.data.items[0].customFields[0].answer.toString();
+    addRole(memberName)
+  });
+
+  
+});
+
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
   ],
 });
 
 const myGuildID = "1081268387604336781";
 let myGuild = null;
 const roleID = "1081313810851319838";
-const memberName = "Jeaaaaaannne !";
 
 function setGuild() {
   myGuild = client.guilds.cache.at(0);
 }
 
-function getMember() {
-  const user = client.users.cache.find(user => user.username == memberName);
+async function getMember(memberName) {
 
+  await myGuild.members.fetch()
+  const user = myGuild.members.cache.find(member => member.user.username == memberName)
+  
   if (user)
   {
     const user_id = user.id;
@@ -27,25 +63,18 @@ function getMember() {
   }
   else
   {
-    throw Error("Member not existing")
+    throw Error("Member not existing :" + memberName+ "*")
   }
+}
+
+async function addRole(memberName) {
+  const member = await getMember(memberName)
+  member.roles.add(roleID)
 }
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
   setGuild()
-})
-
-client.on("messageCreate", (message) => {
-  if (message.content === "ping") {
-    message.reply(({
-      content: "pong",
-    }))
-
-    const member = getMember()
-    member.roles.add(roleID)
-  }
-  
 })
 
 client.login(process.env['TOKEN'])
